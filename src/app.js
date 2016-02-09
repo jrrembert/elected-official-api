d4c709c187b2a7147c42a730e1657e3951830117
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2016-02-09 15:17:50 -0800

    Initial Heroku setup

--- src/app.js
+++ src/app.js
@@ -8,14 +8,17 @@ var abbreviations = require('./mock/abbreviations.json');
 var officials = require('./mock/state_officials.json');
 
 // HTTPS config
-var options = {
-	key: fs.readFileSync('server.key'),
-	cert: fs.readFileSync('server.crt'),
-	requireCert: false,
-	rejectUnauthorized: false
-};
+// var options = {
+// 	key: fs.readFileSync('server.key'),
+// 	cert: fs.readFileSync('server.crt'),
+// 	requireCert: false,
+// 	rejectUnauthorized: false
+// };
 
 var app = express();
+
+// Needed to allow Heroku to set port
+var port = process.env.PORT || 8080;
 
 // Simple attempt to normalize data by extending the native String object.
 // Wouldn't normally play with native types like this, but it's convenient here.
@@ -31,16 +34,16 @@ String.prototype.capitalize = function() {
 		}
 	}
 	return newStr;
-}
+};
 
 app.get('/', function(req, res) {
 	res.send('Welcome to the Elected Officials API!');
-})
+});
 
 app.get('/myapi', function(req, res) {
 	if (!req.query.state) {
 		res.json(officials);
-	} 
+	}
 
 	var official = "";
 
@@ -51,13 +54,13 @@ app.get('/myapi', function(req, res) {
 	} else {
 		official = officials[req.query.state.capitalize()];
 	}
-	
+
 	if (official !== undefined) {
 		res.json(official);
 	} else
-		res.status(400).json({error: req.query.state + ' not found.'})
-})
+		res.status(400).json({error: req.query.state + ' not found.'});
+});
 
-var server = https.createServer(options, app).listen(3000, function() {
-	console.log("Server running on port 3000. Press Ctrl-C to exit.")
-})
+var server = app.listen(port), function() {
+	console.log("Server running on port " + port + ". Press Ctrl-C to exit.");
+});
