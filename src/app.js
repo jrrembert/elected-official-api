c98c2873dceb88fb29413c252e4f1706a96d3c20
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2016-02-16 13:46:52 -0800

    Improve error handling; begin stubbing out db work

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 'use strict';
 
-var https = require('https');
-var fs = require('fs');
 var express = require('express');
+var favicon = require('serve-favicon');
+
+// DB settings
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017')
+
+
 
 var abbreviations = require('./mock/abbreviations.json');
 var officials = require('./mock/state_officials.json');
 
-// HTTPS config
-// var options = {
-// 	key: fs.readFileSync('server.key'),
-// 	cert: fs.readFileSync('server.crt'),
-// 	requireCert: false,
-// 	rejectUnauthorized: false
-// };
+// Serve favicon
+// TODO: is this overkill?
+app.use(favicon(__dirname + '/assets/favicon.ico'));
+
+
 
 var app = express();
 
@@ -35,6 +39,10 @@ String.prototype.capitalize = function() {
 	}
 	return newStr;
 };
+
+
+
+
 
 app.get('/', function(req, res) {
 	res.send('Welcome to the Elected Officials API!');
@@ -61,6 +69,40 @@ app.get('/myapi', function(req, res) {
 		res.status(400).json({error: req.query.state + ' not found.'});
 });
 
-var server = app.listen(port, function() {
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	var err = new Error('404 Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// Error handlers
+
+// Development - print stacktraces
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+// Production - don't display stacktraces
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
+});
+
+
+
+app.listen(port, function() {
 	console.log("Server running on port " + port + ". Press Ctrl-C to exit.");
 });
+
+module.exports = app;
