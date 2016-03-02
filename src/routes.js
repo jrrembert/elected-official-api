216787c48ad33579a3d496c4290fa7030919aede
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2016-03-02 15:39:39 -0800

    Enable passing query parameters to /congress
    * Simple validation performed on `state` and `party` parameters
    * TODO: need tests for various edge cases. Holding off until I know how
    I want to handle normalization of data.

--- src/routes.js
+++ src/routes.js
@@ -44,6 +44,41 @@ var processGovernorQuery = function(query) {
     return query;
 };
 
+var processCongressQuery = function(query) {
+    var newQuery = query;
+
+    if (query.state) {
+        // We want 'state' to be flexible in terms of what it accepts as a
+        // valid parameter as well as keep our search syntax somewhat
+        // similar to /governor queries.
+
+        // We're introducting an intentional bias towards the 'state' param.
+        // If present in 'query', we will attempt to normalize 'newQuery' based
+        // on the value of 'query.state' and update 'newQuery' accordingly.
+        if (query.state.length === 2) {
+            newQuery.state = query.state.toUpperCase();
+            delete newQuery.state_name;
+        } else {
+            newQuery.state_name = query.state.capitalize();
+            delete newQuery.state;
+        }
+    }
+
+    if (query.party){
+        var reRep = /republicans?\b/i;
+        var reDem = /democrats?\b/i;
+        var reInd = /independents?\b/i;
+
+        newQuery.party = query.party.toUpperCase();
+        
+        reRep.test(query.party) ? newQuery.party = "R" : newQuery;
+        reDem.test(query.party) ? newQuery.party = "D" : newQuery;
+        reInd.test(query.party) ? newQuery.party = "I" : newQuery;
+    }
+
+    return newQuery;
+};
+
 exports.appRoot = function(req, res) {
     res.send('Welcome to the Elected Officials API!');
 };
@@ -74,9 +109,18 @@ exports.getGovernorById = function(req, res) {
 exports.getCongressMembers = function(req, res) {
     var db = req.db;
     var collection = db.get(config.get('database.congress_collection'));
-    collection.find({}, fields, function(err, docs) {
-        res.json(queryResults(err, docs));
-    });
+
+    if (_.isEmpty(req.query)) {
+        collection.find({}, fields, function(err, docs) {
+            res.json(queryResults(err, docs));
+        });
+    } else {
+        collection.find(processCongressQuery(req.query), fields,
+            function(err, docs) {
+                res.json(queryResults(err, docs));
+            }
+        );
+    }
 };
 
 exports.getCongressMemberById = function(req, res) {
